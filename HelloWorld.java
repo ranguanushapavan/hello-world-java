d14f88e42c4dc7bbe17f37522dcaf7779ac29961
ranguanushapavan <ranguanushapavan@gmail.com>
2025-07-01 12:05:22 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -35,7 +35,7 @@
 public class HelloWorld {
   public static void main(String[] args) {
      System.out.println("Hello, KrishnaDevOpsss");
-     System.out.println("Hello, AWSB05DevOpss_AWS011");
+     System.out.println("Hello, AWSB05DevOpssss_AWS011");
      System.out.println("Hello, Jenkinnsteamms"); 
 
   }
